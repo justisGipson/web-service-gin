838363b6c596a34c8d100a9380d7e777b70b7b12
Justis <justis710@gmail.com>
2021-08-12 15:25:03 -0400

    it works

--- main.go
+++ main.go
@@ -21,7 +21,46 @@ var albums = []album{
 	{ID: "3", Title: "Enter the Wu-Tang", Artist: "Wu-Tang Clan", Price: 17.99},
 }
 
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
+
 // getAlbums responds with list of all albums as json
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds album from json from req body
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// call BindJSON to bind received json to newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// add newAlbum to slice
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID fetches album with mathing id sent by client
+// returns album as response
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// loop over list of albums, searching for id that matches
+	// parameter sent
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
